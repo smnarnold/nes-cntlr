49a245dcadc675e9d8e40ba3748cbd9f591a0913
Arnold Simon <arnolali@gmail.com>
2018-02-11 18:52:56 -0500

    - Added Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    }]
+  }
+}
+
+module.exports = config
