68bc2e1c8db4ab59289fdce851d4ec0c0c5ffa91
Arnold Simon <arnolali@gmail.com>
2018-02-11 19:17:53 -0500

    - Update webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,25 @@ const webpack = require('webpack')
 const path = require('path')
 
 const config = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+  context: path.resolve(__dirname, 'nes'),
+  entry: {
+    'cntlr': './cntlr.dev.js',
+    'cntlr.min': './cntlr.dev.js',
   },
+  output: {
+    path: path.resolve(__dirname, 'nes'),
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ],
   module: {
     rules: [{
       test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
+      include: path.resolve(__dirname, 'nes'),
       use: [{
         loader: 'babel-loader',
         options: {
